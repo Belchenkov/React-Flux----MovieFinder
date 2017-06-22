4d4853340796babea674f3c3cac1f3a9ba320bb1
Belchenkov <u608110@gmail.com>
2017-06-22 12:47:11 +0300

    Fetch data from Server

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -4,15 +4,38 @@ var AppStore = require('../stores/AppStore');
 var SearchForm = require('./SearchForm');
 
 
+function getAppState() {
+	return {
+		movies: AppStore.getMovieResults()
+	}
+}
+
 var App = React.createClass({
 
+	getInitialState: function() {
+		return getAppState();
+	},
+
+	componentDidMount: function () {
+		AppStore.addChangeListener(this._onChange);
+	},
+
+	componentWillUnmount: function () {
+		AppStore.removeChangeListener(this._onChange);
+	},
+
 	render: function(){
+		console.log(this.state.movies);
 		return(
 			<div>
 				<SearchForm />
 			</div>
 		);
 	},
+
+	_onChange: function () {
+		this.setState(getAppState());
+	}
 
 });
 
--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -1,7 +1,17 @@
 var AppActions = require('../actions/AppActions');
 
 module.exports = {
-	searchMovies: function () {
-        
+	searchMovies: function (movie) {
+        $.ajax({
+            url: 'http://www.omdbapi.com/?s=' + movie.title + '&apikey=2bfc27b0',
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                AppActions.receiveMovieResults(data.Search);
+            }.bind(this),
+            error: function (xhr, status, err) {
+                alert(err);
+            }.bind(this)
+        });
     }
 }
