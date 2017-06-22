f376a6b76ac21adf77fcb4b8bba0fbc3d6c565e8
Belchenkov <u608110@gmail.com>
2017-06-22 13:23:12 +0300

    Finish Project

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,40 +2,47 @@ var React = require('react');
 var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
 var SearchForm = require('./SearchForm');
+var MovieResults = require('./MovieResults');
 
-
-function getAppState() {
-	return {
-		movies: AppStore.getMovieResults()
-	}
+function getAppState(){
+    return {
+        movies: AppStore.getMovieResults()
+    }
 }
 
 var App = React.createClass({
 
-	getInitialState: function() {
-		return getAppState();
-	},
+	getInitialState: function(){
+        return getAppState();
+    },
 
-	componentDidMount: function () {
-		AppStore.addChangeListener(this._onChange);
-	},
+    componentDidMount: function(){
+        AppStore.addChangeListener(this._onChange);
+    },
 
-	componentWillUnmount: function () {
-		AppStore.removeChangeListener(this._onChange);
-	},
+    componentWillUnmount: function(){
+        AppStore.removeChangeListener(this._onChange);
+    },
 
 	render: function(){
 		console.log(this.state.movies);
-		return(
-			<div>
-				<SearchForm />
-			</div>
-		);
+		  if(this.state.movies == ''){
+            var movieResults = ''
+        } else {
+            var movieResults = <MovieResults movies={this.state.movies} />
+        }
+
+		 return(
+            <div>
+                <SearchForm />
+                {movieResults}
+            </div>
+        )
 	},
 
-	_onChange: function () {
-		this.setState(getAppState());
-	}
+	 _onChange: function(){
+        this.setState(getAppState());
+    }
 
 });
 
--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -1,4 +1,4 @@
-var AppDispatcher = require('../dispatcher/AppDispatcher');
+var AppDispatcher =  require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
@@ -11,41 +11,42 @@ var _selected = '';
 
 var AppStore = assign({}, EventEmitter.prototype, {
 
-	setMovieResults: function (movies) {
-		_movies = movies;
-	},
-
-	getMovieResults: function () {
-		return _movies;
-	},
-
-	emitChange: function(){
-		this.emit(CHANGE_EVENT);
-	},
-	addChangeListener: function(callback){
-		this.on('change', callback);
-	},
-	removeChangeListener: function(callback){
-		this.removeListener('change', callback);
-	}
+    setMovieResults: function(movies){
+        _movies = movies;
+    },
+    getMovieResults: function(){
+        return _movies;
+    },
+    emitChange: function(){
+        this.emit(CHANGE_EVENT);
+    },
+    addChangeListener: function(callback){
+        this.on('change', callback);
+    },
+    removeChangeListener: function(callback){
+        this.removeListener('change', callback);
+    }
 });
+
 
 AppDispatcher.register(function(payload){
-	var action = payload.action;
+    var action = payload.action;
 
-	switch(action.actionType){
-		case AppConstants.SEARCH_MOVIES:
-			console.log('Searching for movie ' + action.movie.title);
-		AppAPI.searchMovies(action.movie);
-		AppStore.emit(CHANGE_EVENT);
-		break;
-	case AppConstants.RECEIVE_MOVIE_RESULTS:
-		AppStore.setMovieResults(action.movies);
-		AppStore.emit(CHANGE_EVENT);
-		break;
-}
+    switch(action.actionType){
+        case AppConstants.SEARCH_MOVIES:
+            console.log('Searching for movie ' + action.movie.title);
+            AppAPI.searchMovies(action.movie);
+            AppStore.emit(CHANGE_EVENT);
+            break;
 
-	return true;
+        case AppConstants.RECEIVE_MOVIE_RESULTS:
+            AppStore.setMovieResults(action.movies);
+            AppStore.emit(CHANGE_EVENT);
+            break;
+    }
+
+    return true;
 });
+
 
 module.exports = AppStore;
